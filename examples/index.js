6383b235cd2b1d09ac5f0ca0c0698e61b7e58419
Jed Watson <jed.watson@me.com>
2017-12-18 10:53:40 +1100

    Adding mvp examples implementation

---
+++ examples/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+// import Value from 'react-value';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'examples'),
+  entry: {
+    index: './index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, '.examples'),
+    filename: '[name].js',
+    publicPath: '/',
+  },
+  devServer: {
+    port: 8000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      'react-value': path.resolve(__dirname, 'src/index'),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: false,
+      template: path.resolve(__dirname, 'examples/index.html'),
+    }),
+  ],
+};
