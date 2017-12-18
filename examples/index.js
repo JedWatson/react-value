7281ee256501a30502f65c5ac7d2499a94833450
Jed Watson <jed.watson@me.com>
2017-12-18 11:25:43 +1100

    Adding complete examples

--- examples/index.js
+++ examples/index.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-// import Value from 'react-value';
+import { Value, withValue } from 'react-value';
 import './index.css';
+
+const inc = i => (i || 0) + 1;
+
+const logChange = name => value =>
+  console.log(`${name} value updated: ${value}`);
+
+class Input extends Component {
+  onClick = () => {
+    const { value } = this.props;
+    this.props.onChange(inc(value));
+  };
+  render() {
+    const { value } = this.props;
+    return (
+      <div>
+        <pre>{value || 0}</pre>
+        <button onClick={this.onClick}>increment</button>
+      </div>
+    );
+  }
+}
+
+const AltProps = ({ altValue, altOnChange, ...props }) => (
+  <Input value={altValue} onChange={altOnChange} {...props} />
+);
+
+const AltPropsWithValue = withValue(AltProps, {
+  valueProp: 'altValue',
+  onChangeProp: 'altOnChange',
+});
+
+const InputWithValue = withValue(Input);
 
 class App extends Component {
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div>
+        <h3>Basic Example:</h3>
+        <Value
+          onChange={logChange('Basic')}
+          render={(value, onChange) => (
+            <Input value={value} onChange={onChange} />
+          )}
+        />
+        <h3>Default Value:</h3>
+        <Value
+          defaultValue={3}
+          onChange={logChange('With default')}
+          render={(value, onChange) => (
+            <Input value={value} onChange={onChange} />
+          )}
+        />
+        <h3>Higher Order Component:</h3>
+        <InputWithValue onChange={logChange('HOC')} defaultValue={5} />
+        <h3>Alternate Prop Names:</h3>
+        <AltPropsWithValue onChange={logChange('Alt Props')} defaultValue={8} />
+      </div>
+    );
   }
 }
 
