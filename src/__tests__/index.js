d2c4749c74191bd6453e33899cbd88f13bf78045
Trey Shugart <treshugart@gmail.com>
2017-12-21 15:53:42 +1100

    Add tests (currently failing).

---
+++ src/__tests__/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Value, withValue } from '..';
+
+function render(spy) {
+	return function({ onChange, value }) {
+		spy(value);
+		return <input onChange={onChange} value={value} />;
+	};
+}
+
+test('Value - should call onChange', () => {
+	const spy = jest.fn();
+	const val = shallow(<Value render={render(spy)} defaultValue={1} />);
+	expect(spy).toHaveBeenCalledTimes(1);
+	expect(spy).toHaveBeenCalledWith(1);
+	val.find('input').simulate('change', 2);
+	expect(spy).toHaveBeenCalledTimes(2);
+	expect(spy).toHaveBeenCalledWith(2);
+});
