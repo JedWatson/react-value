4ac8352295a9e598a3c2cef8882e68e777587f89
Trey Shugart <treshugart@gmail.com>
2017-12-21 16:03:20 +1100

    Make tests pass by changing API they were expecting.

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -3,18 +3,18 @@ import { shallow } from 'enzyme';
 import { Value, withValue } from '..';
 
 function render(spy) {
-	return function({ onChange, value }) {
-		spy(value);
-		return <input onChange={onChange} value={value} />;
-	};
+  return function(value, onChange) {
+    spy(value);
+    return <input onChange={onChange} value={value} />;
+  };
 }
 
 test('Value - should call onChange', () => {
-	const spy = jest.fn();
-	const val = shallow(<Value render={render(spy)} defaultValue={1} />);
-	expect(spy).toHaveBeenCalledTimes(1);
-	expect(spy).toHaveBeenCalledWith(1);
-	val.find('input').simulate('change', 2);
-	expect(spy).toHaveBeenCalledTimes(2);
-	expect(spy).toHaveBeenCalledWith(2);
+  const spy = jest.fn();
+  const val = shallow(<Value render={render(spy)} defaultValue={1} />);
+  expect(spy).toHaveBeenCalledTimes(1);
+  expect(spy).toHaveBeenCalledWith(1);
+  val.find('input').simulate('change', 2);
+  expect(spy).toHaveBeenCalledTimes(2);
+  expect(spy).toHaveBeenCalledWith(2);
 });
