d8233f5704c8671407c8b3505ee471aaea4dc3f3
Trey Shugart <treshugart@gmail.com>
2018-01-31 12:12:07 +1100

    Spelling on a comment.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import React, { Component } from 'react';
 
 // This is similar to { ...state, ...props } but it doesn't
-// include prop values that arent in the current state.
+// include prop values that aren't in the current state.
 function getOverriddenState(props: Object, state: Object) {
   return Object.keys(state).reduce((prev, next) => {
     prev[next] = next in props ? props[next] : state[next];
